1170de7d1db3d80aa4d2b769a959f506f31fe2fa
BOTTE Valentin <vale.007@hotmail.fr>
2016-11-23 16:22:52 +0100

    création classe bdd

---
+++ php/Bdd.php
@@ -0,0 +1,29 @@
+<?php
+
+
+	class Bdd{
+		private $_dbmysql;
+
+		public function __construct(){
+			try{
+
+				$connect_str = "mysql:host=localhost;dbname=clientsoo";
+				$connect_user = "root";
+				$connect_pass = "":
+
+				$options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+				$this->_dbmysql = new PDO($connect_str, $connect_user, $connect_pass, $options);
+
+			}catch(Exception $e){
+				throw new  Exception("Erreur à la connexion", 1);
+			}
+
+		}
+
+		public function requeteSQL($sql){
+
+		}
+	
+	}
+
+?>
