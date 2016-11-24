7bff5a68fae69dfde8f3d29c0a802e368c97d1e5
BOTTE Valentin <vale.007@hotmail.fr>
2016-11-24 11:49:51 +0100

    avancé insert

--- classes/AbstractRepository.php
+++ classes/AbstractRepository.php
@@ -27,8 +27,19 @@
 			while($data = $req->fetchObject($this->classe)){
 				$clients[] = $data;
 			}
-			
+
 			return $clients;
+
+		}
+
+		public function insert($objet){
+
+			$req = $this->bdd->requeteSQL('INSERT INTO ' . $this->classe . ' (TITRE, NOM, PRENOM, ADRESSERUE1, ADRESSERUE2, CP, VILLE, TEL) VALUE (:titre, :nom, :prenom, :adresserue1, :adresserue2, :cp, :ville, :tel)');
+
+			$req->b
+
+
+
 
 		}
 
